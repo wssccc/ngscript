9bc32ce9a99ba33b264239338ec0d9ebf158614c
wssccc <wssccc@qq.com>
2014-09-16 21:49:04 +0800

    save sth

--- src/ngscript/vm/Coroutine.java
+++ src/ngscript/vm/Coroutine.java
@@ -6,7 +6,6 @@ package ngscript.vm;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
