bfd90c9ac89ade8f96a290904b7e5310374fd794
wssccc <wssccc@qq.com>
2021-07-10 22:36:28 +0800

    Update copyright

--- ngscript-core/src/main/java/org/ngscript/fastlexer/Lexer.java
+++ ngscript-core/src/main/java/org/ngscript/fastlexer/Lexer.java
@@ -1,6 +1,19 @@
 /*
- *  wssccc all rights reserved
+ * Copyright 2021 wssccc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.ngscript.fastlexer;
 
 import org.ngscript.parseroid.parser.Token;
@@ -11,8 +24,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- *
- * @author wssccc <wssccc@qq.com>
+ * @author wssccc
  */
 public class Lexer {
     private static Set<String> keywords = new HashSet<>(Arrays.asList("var", "true", "false", "null", "undefined",
--- ngscript-core/src/main/java/org/ngscript/runtime/vo/FunctionDef.java
+++ ngscript-core/src/main/java/org/ngscript/runtime/vo/FunctionDef.java
@@ -1,14 +1,26 @@
 /*
- *  wssccc all rights reserved
+ * Copyright 2021 wssccc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.ngscript.runtime.vo;
 
 import org.ngscript.runtime.Environment;
 import org.ngscript.runtime.VirtualMachine;
 
 /**
- *
- * @author wssccc <wssccc@qq.com>
+ * @author wssccc
  */
 public class FunctionDef {
 
